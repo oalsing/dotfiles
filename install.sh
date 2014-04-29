07bcd1728a0f29a16160f59b083169a0034cb4a2
oalsing <oscar.alsing@gmail.com>
2014-04-29 12:31:11 +0200

    adds install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# installs most common config files
+cd
+rm -r .vim .vimrc .tmux.conf .zshrc .gitconfig
+ln -s dotfiles/vimrc .vimrc
+ln -s dotfiles/tmux.conf .tmux.conf
+ln -s dotfiles/zshrc .zshrc
+ln -s dotfiles/gitconfig .gitconfig
+ln -s dotfiles/vim .vim
+cd dotfiles
+./install_gnome_solarized.sh
