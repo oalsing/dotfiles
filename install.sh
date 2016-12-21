dd07ee4036b558911ed896dc2acb32a5263707a1
oalsing <oscar.alsing@gmail.com>
2016-12-21 15:18:32 +0100

    removes weechat and updates readme

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 # installs most common config files
 cd
-rm -r .vim .vimrc .tmux.conf .zshrc .gitconfig .weechat/weechat.conf .weechat/irc.conf
+rm -r .vim .vimrc .tmux.conf .zshrc .gitconfig
 ln -s dotfiles/vimrc .vimrc
 ln -s dotfiles/tmux.conf .tmux.conf
 ln -s dotfiles/zshrc .zshrc
 ln -s dotfiles/gitconfig .gitconfig
 ln -s dotfiles/vim .vim
-mkdir .weechat
-ln -s dotfiles/weechat.conf .weechat/weechat.conf
-ln -s dotfiles/irc.conf .weechat/irc.conf
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
